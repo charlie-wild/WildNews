c849fcdbcbd77e3f16212d724273696919f12e8a
charlie-wild <charliewild@gmail.com>
2019-01-10 11:40:26 +0000

    styling header and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,12 +7,43 @@ class Navbar extends Component {
     render() {
     const { topics } = this.props;
     return (
-      <div className="navbar">
-        {topics.map(topic => {
-          return <span key={topic.slug}>
+      <div class='navbar' role='navigation' aria-label="main navigation" className="navbar">
+        <div class='navbar brand' >
+          <div role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+           <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </div>
+        </div>
+        <div class='navbar-start'>
+        <div class='navbar-item'>
+          {<Link to='/'>Home</Link>}
+        </div>
+        <div class='navbar-item has-dropdown is-hoverable'>
+        <div class='navbar-link'>
+          Topics
+        </div>
+        <div class='navbar-dropdown'>{topics.map(topic => {
+          return <span key={topic.slug} class='navbar-item'>
             <Link to={`/${topic.slug}`}>{topic.slug}</Link>
           </span>
         })}
+      </div>
+      </div>
+      </div>
+        <div class='navbar-end'>
+          <div class='navbar-item'>
+            <div class='buttons'>
+              <div class='button is-light'>
+                {<Link to='/create_topic'>Create Topic</Link>}
+              </div>
+              <div class='button is-light'>
+                {<Link to='/create_article'>Create Article</Link>}
+              </div>
+
+            </div>
+          </div>
+        </div>
       </div>
     
     );
