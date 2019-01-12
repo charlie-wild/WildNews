48e5efe47dac03811446308096982ef2aac9158c
charlie-wild <charliewild@gmail.com>
2019-01-12 10:36:39 +0000

    fixing issues with login button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import React, { Fragment} from 'react';
 import './header.css';
 
-const Header = (logout) => {
+const Header = ({logout}) => {
     return (
     <Fragment>
     <header><h1 className='title is-1'>NC News</h1></header>
     <div className='user_info'>
-      <button className='button is-danger' onClick={logout}>Logout</button>
+      <button className='button is-danger' onClick={ logout }>Logout</button>
     </div>
     </Fragment>
 
